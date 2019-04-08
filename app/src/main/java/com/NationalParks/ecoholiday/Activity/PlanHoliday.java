f54ff71aa8252e18e09ae1438ee5b561bf797b51
ecoholiday <ecoholiday.teamteam@gmail.com>
2019-04-08 23:49:21 +1000

    no changes are made

--- app/src/main/java/com/NationalParks/ecoholiday/Activity/PlanHoliday.java
+++ app/src/main/java/com/NationalParks/ecoholiday/Activity/PlanHoliday.java
@@ -1,6 +1,7 @@
 package com.NationalParks.ecoholiday.Activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,12 +22,13 @@ public class PlanHoliday extends AppCompatActivity {
         imgNationalPark.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //imgNationalPark.setBackgroundColor(Color.parseColor("#000000"));
                 Intent intentNP = new Intent(PlanHoliday.this, MainActivity.class);
                 startActivity(intentNP);
             }
         });
 
-        final ImageView imgStats = (ImageView)findViewById(R.id.imgStats);
+        ImageView imgStats = (ImageView)findViewById(R.id.imgStats);
         imgStats.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,26 +42,31 @@ public class PlanHoliday extends AppCompatActivity {
         imgHiking.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intentStats = new Intent(PlanHoliday.this, Cycling.class);
-                startActivity(intentStats);
+                Intent intent = new Intent(PlanHoliday.this, Cycling.class);
+                startActivity(intent);
             }
         });
-        ImageView imgCycling = (ImageView)findViewById(R.id.imgCycling);
+
+        final ImageView imgCycling = (ImageView)findViewById(R.id.imgCycling);
         imgCycling.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intentStats = new Intent(PlanHoliday.this, Cycling.class);
-                startActivity(intentStats);
+                imgCycling.setBackgroundColor(Color.parseColor("#25368A"));;
+                //Intent intent = new Intent(PlanHoliday.this, Cycling.class);
+                //startActivity(intent);
             }
         });
+
+
         ImageView imgCamping = (ImageView)findViewById(R.id.imgCamping);
         imgCamping.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intentStats = new Intent(PlanHoliday.this, Cycling.class);
-                startActivity(intentStats);
+                Intent intent = new Intent(PlanHoliday.this, Cycling.class);
+                startActivity(intent);
             }
         });
+
 
     }
 }
